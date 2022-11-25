b9bbecde100165c7255a2cb0a55eeef609c43248
triabagus <triatop9@gmail.com>
2022-11-25 22:42:25 +0700

    dark theme variable

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,6 +71,14 @@ const scrollActive = () => {
 window.addEventListener('scroll', scrollActive)
 
 /*=============== DARK LIGHT THEME ===============*/ 
+const themeButton   = document.getElementById('theme-button')
+const darkTheme     = 'dark-theme'
+const iconTheme     = 'bx-sun'
 
+// Previously selected topic (if user selected)
+const selectedtheme     = localStorage.getItem('selected-theme')
+const selectedIcon      = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
